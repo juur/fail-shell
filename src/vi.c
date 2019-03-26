219faf3cc818cf86db22f1912b3b63ec00ead449
juur <juur@users.noreply.github.com>
2019-03-26 22:46:43 +0000

    blah

--- src/vi.c
+++ src/vi.c
@@ -9,61 +9,15 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <libgen.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/ioctl.h>
 
 #define	KEY_ESC	033
 #define KEY_HT	'\t'
 #define	KEY_CR	'\r'
 
-#define CTRL(x) ((x)&31)
-
-inline static ssize_t min(const ssize_t a, const ssize_t b)
-{
-	return (a < b) ? (a) : (b);
-}
-
-inline static ssize_t max(const ssize_t a, const ssize_t b)
-{
-	return (a > b) ? (a) : (b);
-}
-
-
-static void done(int sig)
-{
-	exit(EXIT_FAILURE + sig);
-}
-
-static void clean(void)
-{
-	echo();
-	noraw();
-	nl();
-	endwin();
-}
-
-static void init(void)
-{
-	signal(SIGINT, done);
-	signal(SIGQUIT, done);
-	//signal(SIGILL, done);
-	signal(SIGKILL, done);
-	signal(SIGTERM, done);
-	//signal(SIGSEGV, done);
-
-	setvbuf(stderr, NULL, _IONBF, 0);
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stdin, NULL, _IONBF, 0);
-
-	initscr();
-	atexit(clean);
-
-	nonl();
-	raw();
-	noecho();
-	meta(stdscr, true);
-	idlok(stdscr, true);
-	scrollok(stdscr, false);
-	keypad(stdscr, true);
-}
+#define CTRLCODE(x) ((x)&31)
 
 typedef struct {
 	char	*line;
@@ -78,6 +32,99 @@ typedef struct {
 	char	 *name;
 	bool	  modified;
 } buffer_t;
+
+enum edit_mode_en { EM_NULL=0, EM_CMD, EM_EDIT, EM_LINE };
+
+static buffer_t *cur_buffer = NULL;
+static line_t *cur_line = NULL;
+static ssize_t scr_width = 80, scr_height = 25;
+static ssize_t max_scr_x, max_scr_y;
+static ssize_t file_y = 0, file_x = 0;
+static ssize_t curs_x = 0, curs_y = 0;
+static int tabstop = 8;
+static enum edit_mode_en edit_mode = EM_EDIT;
+static enum edit_mode_en prev_mode = EM_NULL;
+static char edit_line_buf[BUFSIZ]	= {0};
+static char *edit_line_ptr			= NULL;
+
+inline static ssize_t min(const ssize_t a, const ssize_t b)
+{
+	return (a < b) ? (a) : (b);
+}
+
+inline static ssize_t max(const ssize_t a, const ssize_t b)
+{
+	return (a > b) ? (a) : (b);
+}
+
+static void clean(void)
+{
+	static bool done = false;
+
+	if (done) 
+		return;
+
+	echo();
+	nocbreak();
+	nl();
+	endwin();
+	done = true;
+}
+
+static void done(int sig)
+{
+	clean();
+	errx(EXIT_FAILURE, "exiting with signal %d", sig);
+}
+
+static void resize(int sig)
+{
+	struct winsize size;
+	if (ioctl(0, TIOCGWINSZ, (char *)&size) == -1)
+		err(1, "ioctl: TIOCGWINSZ");
+
+	scr_width = size.ws_col;
+	max_scr_x = scr_width - 1;
+	
+	scr_height = size.ws_row;
+	max_scr_y = scr_height - 2;
+
+	// FIXME need to have all instances of curs_y/file_y/cur_line setting
+	// in a function
+	curs_x = min(max_scr_x, curs_x);
+	curs_y = min(max_scr_y, curs_y);
+	cur_line = cur_buffer->lines[file_y + curs_y];
+
+	warnx("resizing to %lu x %lu\n", scr_width, scr_height);
+}
+
+static void init(void)
+{
+	signal(SIGINT, done);
+	signal(SIGQUIT, done);
+	signal(SIGILL, done);
+	signal(SIGKILL, done);
+	signal(SIGTERM, done);
+	signal(SIGSEGV, done);
+
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	initscr();
+	atexit(clean);
+	cbreak();
+	halfdelay(1);
+	noecho();
+	nonl();
+
+	if (meta(stdscr, true) == ERR)
+		errx(1, "meta");
+	if (idlok(stdscr, true) == ERR)
+		warnx("idleok");
+	if (scrollok(stdscr, false) == ERR)
+		errx(1, "scrollok");
+	if (keypad(stdscr, true) == ERR)
+		errx(1, "keypad");
+}
 
 static buffer_t *readfile(FILE *restrict f, char *restrict name)
 {
@@ -113,7 +160,7 @@ static buffer_t *readfile(FILE *restrict f, char *restrict name)
 
 		if ((ret->lines[pos] = malloc(sizeof(line_t))) == NULL)
 			goto warnfail;
-		
+
 		ret->lines[pos]->size = len + 1;
 		ret->lines[pos]->used = len;
 
@@ -154,19 +201,6 @@ warnfail:
 	warn("readfile");
 	goto fail;
 }
-
-enum edit_mode_en { EM_NULL=0, EM_CMD, EM_EDIT, EM_LINE };
-
-static buffer_t *cur_buffer = NULL;
-static line_t *cur_line = NULL;
-static ssize_t scr_width = 80, scr_height = 25;
-static ssize_t max_scr_x, max_scr_y;
-static ssize_t file_y = 0, file_x = 0;
-static ssize_t curs_x = 0, curs_y = 0;
-static int tabstop = 8;
-static enum edit_mode_en edit_mode = EM_EDIT;
-static enum edit_mode_en prev_mode = EM_NULL;
-
 inline static int next_tab(const ssize_t offset_x)
 {
 	if ( (offset_x+1) % tabstop == 0)
@@ -176,11 +210,11 @@ inline static int next_tab(const ssize_t offset_x)
 
 static void draw(void)
 {
-	for (ssize_t y = 0; y < scr_height; y++)
+	for (ssize_t y = 0; y <= max_scr_y; y++)
 	{
 		if ( (file_y + y) > cur_buffer->used) 
 			break;
-		
+
 		const line_t *line = cur_buffer->lines[file_y + y];
 		if (!line || !line->line) 
 			continue;
@@ -188,16 +222,16 @@ static void draw(void)
 		for (ssize_t scr_x = 0, x = 0; x < line->used-1; x++)
 		{
 			if (scr_x >= max_scr_x && (scr_x - file_x) >= max_scr_x) {
-				mvaddch(y, max_scr_x, '$');
+				mvwaddch(stdscr, y, max_scr_x, '$');
 				break;
 			}
 
 			const char c = line->line[x];
 
-			if (c == '\t') {
+			if (c == KEY_HT) {
 				scr_x += next_tab(scr_x);
 			} else if (isprint(c)) {
-				mvaddch(y, scr_x - file_x, c);
+				mvwaddch(stdscr, y, scr_x - file_x, c);
 				scr_x++;
 			} else {
 				scr_x++;
@@ -207,8 +241,8 @@ static void draw(void)
 	}
 }
 
-/* find the character offset in the string we are at, even if we 'join' the line half
- * way through a tab */
+/* find the character offset in the string we are at, even if 
+ * we 'join' the line half way through a tab */
 static ssize_t compute_linex(void)
 {
 	const ssize_t target = file_x + curs_x;
@@ -220,7 +254,7 @@ static ssize_t compute_linex(void)
 		if (off > target) return x;
 		/* this first check is needed of we press up and go beyond 
 		 * the end of line */
-		if (( x< cur_line->used-1) && cur_line->line[x] == '\t') {
+		if (( x< cur_line->used-1) && cur_line->line[x] == KEY_HT) {
 			off += next_tab(off);
 		} else {
 			off++;
@@ -241,7 +275,8 @@ static void clampx(void)
 		file_x += curs_x - max_scr_x; 
 		curs_x = max_scr_x; 
 	}
-	//if (file_x > ((cur_line->used-1) - (scr_width-1))) file_x = (cur_line->used-1) - (scr_width-1);
+	//if (file_x > ((cur_line->used-1) - (scr_width-1))) 
+	//file_x = (cur_line->used-1) - (scr_width-1);
 	if (file_x < 0) 
 		file_x = 0;
 }
@@ -255,20 +290,19 @@ static void move_up(void)
 
 	cur_line = cur_buffer->lines[file_y + curs_y];
 	clampx();
-	move(curs_y, curs_x);
+	wmove(stdscr, curs_y, curs_x);
 }
 
 static void move_down(void)
 {
-	if (curs_y == (scr_height - 1) && 
-			(file_y + scr_height < cur_buffer->used)) 
+	if (curs_y == max_scr_y && (file_y + max_scr_y < cur_buffer->used-1)) 
 		file_y++;
-	else if (curs_y < (scr_height - 1)) 
+	else if ((curs_y < max_scr_y) && (curs_y + file_y < cur_buffer->used-1))
 		curs_y++;
 
-	cur_line = cur_buffer->lines[file_y + curs_y];
+	cur_line = cur_buffer->lines[min(file_y + curs_y, cur_buffer->used - 1)];
 	clampx();
-	move(curs_y, curs_x);
+	wmove(stdscr, curs_y, curs_x);
 }
 
 /* convert a character offset in a string to an absolute screen offset */
@@ -280,7 +314,7 @@ static ssize_t compute_offsetx(const ssize_t pos)
 	{
 		if (x == pos) 
 			return off;
-		if (cur_line->line[x] == '\t') {
+		if (cur_line->line[x] == KEY_HT) {
 			off += next_tab(off);
 		} else {
 			off++;
@@ -302,7 +336,7 @@ static void move_left(void)
 
 	clampx();
 
-	move(curs_y, curs_x);
+	wmove(stdscr, curs_y, curs_x);
 }
 
 static void move_right(void)
@@ -318,7 +352,7 @@ static void move_right(void)
 
 	clampx();
 
-	move(curs_y, curs_x);
+	wmove(stdscr, curs_y, curs_x);
 }
 
 static void insert_line(const bool shift, line_t *newline)
@@ -395,10 +429,10 @@ static void delete_char(const int pos)
 
 			const line_t *cur = cur_buffer->lines[line];
 			line_t *prv = cur_buffer->lines[line-1];
-			
+
 			size_t old = (prv->used-1);
 			const size_t len = (cur->used-1) + (prv->used) + 1;
-			
+
 			if ((merged = malloc(len)) == NULL)
 				err(1, "delete_char");
 
@@ -421,7 +455,9 @@ static void delete_char(const int pos)
 
 	cur_buffer->modified = true;
 	if(pos<0) move_left();
-	memmove(cur_line->line+point, cur_line->line+point+1, cur_line->used - (point));
+	memmove(cur_line->line+point, 
+			cur_line->line+point+1, 
+			cur_line->used - (point));
 
 	cur_line->used--;
 }
@@ -430,7 +466,7 @@ static void insert_char(const char c)
 {
 	cur_buffer->modified = true;
 
-	if (c == '\r') {
+	if (c == KEY_CR) {
 		if (curs_x + file_x == 0) {
 			// new line, cursor at start
 			insert_line(true, NULL);
@@ -445,7 +481,7 @@ static void insert_char(const char c)
 			cur_line->line[linex+1] = '\0';
 
 			cur_line->used = strlen(cur_line->line); // TODO is this linex?
-			
+
 			line_t *nl = malloc(sizeof(line_t));
 			if (nl == NULL) 
 				err(1, "insert char");
@@ -453,7 +489,7 @@ static void insert_char(const char c)
 			nl->line = new_string;
 			nl->used = strlen(new_string);
 			nl->size = nl->used + 1;
-			
+
 			insert_line(false, nl);
 			curs_x = file_x = 0;
 		}
@@ -462,7 +498,6 @@ static void insert_char(const char c)
 
 	if (cur_line->used == cur_line->size) {
 		const size_t len = (cur_line->size + 128) & ~127;
-		warnx("growing from %lu to %lu", cur_line->size, len);
 		char *tmp = realloc(cur_line->line, len);
 		if (tmp == NULL)
 			err(1, "insert_char");
@@ -488,17 +523,14 @@ static int execute_cmd(const char *str)
 	return 0;
 }
 
-static char edit_line_buf[BUFSIZ]	= {0};
-static char *edit_line_ptr			= NULL;
-
-static void process_line_ch(const char ch)
+static void process_line_ch(const int ch)
 {
 	switch(ch)
 	{
-		case CTRL('C'):
+		case CTRLCODE('C'):
 			edit_mode = prev_mode;
 			break;
-		case '\r':
+		case KEY_CR:
 			if (execute_cmd(edit_line_buf)) {
 				beep();
 				// handle error
@@ -512,24 +544,44 @@ static void process_line_ch(const char ch)
 	}
 }
 
-static void process_cmd(const char ch)
+static void process_cmd_ch(const int ch)
 {
 	switch(ch)
 	{
+		case 'j':
+		case KEY_DOWN:
+			move_down();
+			break;
+		case 'k':
+		case KEY_UP:
+			move_up();
+			break;
+		case 'h':
+		case KEY_BACKSPACE:
+		case KEY_LEFT:
+			move_left();
+			break;
+		case 'l':
+		case KEY_RIGHT:
+			move_right();
+			break;
 		case 'a':
 			move_right();
 			goto insert_mode;
 		case '^':
+		case KEY_HOME:
 			curs_x = 0;
 			file_x = 0;
 			clampx();
 			break;
 		case '$':
+		case KEY_END:
 		case 'A':
-			curs_x = cur_line->used;
+			curs_x = cur_line ? cur_line->used : curs_x;
 			clampx();
 			if (ch == 'A') goto insert_mode;
 			break;
+		case KEY_DL:
 		case 'x':
 			delete_char(0);
 			break;
@@ -549,11 +601,15 @@ insert_mode:
 			prev_mode = EM_CMD;
 			edit_mode = EM_EDIT;
 			break;
+		case CTRLCODE('C'):
+			exit(EXIT_SUCCESS);
+			break;
 		default:
-			move(25, 0);
-			clrtoeol();
-			mvprintw(25, 0, "Error: %c: unknown command", ch);
-			refresh();
+			wmove(stdscr, max_scr_y+1, 0);
+			wclrtoeol(stdscr);
+			mvwprintw(stdscr, max_scr_y+1, 0, 
+					"Error: %c: unknown command", ch);
+			wrefresh(stdscr);
 			beep();
 			sleep(2);
 			break;
@@ -565,22 +621,69 @@ static void print_loc(void)
 	const ssize_t actx = compute_linex();
 
 	if (file_x+curs_x != actx) {
-		mvprintw(25, max_scr_x - 18, "%lu,%lu-%lu",
+		mvwprintw(stdscr, max_scr_y+1, max_scr_x - 18, "%lu,%lu-%lu",
 				file_y+curs_y,
 				actx,
 				file_x+curs_x);
 	} else {
-		mvprintw(25, max_scr_x - 18, "%lu,%lu",
+		mvwprintw(stdscr, max_scr_y+1, max_scr_x - 18, "%lu,%lu",
 				file_y+curs_y,
 				actx);
 	}
 }
 
+static void process_edit_ch(const int ch)
+{
+	switch (ch)
+	{
+		case KEY_DOWN: 
+			move_down();
+			break;
+		case KEY_UP: 
+			move_up();
+			break;
+		case KEY_LEFT: 
+			move_left();
+			break;
+		case KEY_RIGHT:
+			move_right();
+			break;
+		case KEY_HOME: 
+			curs_x = 0; 
+			file_x = 0; 
+			clampx();
+			break;
+		case KEY_END: 
+			curs_x = cur_line ? cur_line->used : curs_x; 
+			clampx();
+			break;
+		case KEY_BACKSPACE:
+			delete_char(-1);
+			break;
+		case KEY_DL:
+			delete_char(0);
+			break;
+		case KEY_ESC:
+			edit_mode = EM_CMD;
+			break;
+		case CTRLCODE('C'):
+			exit(EXIT_SUCCESS);
+		default:
+			insert_char(ch);
+			break;
+	}
+}
+
+static int process_esc_sequence(const int *restrict buf)
+{
+	return ERR;
+}
+
 int main(const int argc, const char *restrict argv[])
 {
-	const char *name;
+	const char *name = (argc>1) ? argv[1] : "/etc/passwd";
 
-	FILE *f = fopen((name = (argc>1) ? argv[1] : "/etc/passwd"), "r");
+	FILE *f = fopen(name, "r");
 	cur_buffer = readfile(f, strdup(name));
 	if (!cur_buffer)
 		errx(1, "unable to read file");
@@ -589,133 +692,96 @@ int main(const int argc, const char *restrict argv[])
 		cur_line = cur_buffer->lines[0];
 
 	init();
+
+	getmaxyx(stdscr, scr_height, scr_width);
+
 	max_scr_x = scr_width - 1;
-	max_scr_y = scr_height - 1;
+	max_scr_y = scr_height - 2;
 
-	erase();
+	werase(stdscr);
 	draw();
-	move(curs_y,curs_x);
-	refresh();
+	wmove(stdscr,curs_y,curs_x);
+	wrefresh(stdscr);
 
-	int ch = 0;
-	goto first;
+	int	buf[BUFSIZ];
+	int pos = 0;
+
+	const struct timespec ts = {
+		.tv_sec = 0,
+		/* 1ms or 1000μs */
+		.tv_nsec = 1000 * 1000
+	};
+
+	bool esc_capture = false;
 
 	while (1)
 	{
-		ch = getch();
-first:
-		erase();
-		//fprintf(stderr, "read: %x\n", ch);
+		int ch = wgetch(stdscr);
 
-		if (edit_mode == EM_LINE) {
-			process_line_ch(ch);
-		} 
-		else switch (ch)
+		/* we should only do this here for ESC sequence processing 
+		 * instead, process_cmd_ch should concat chars if valid, e.g.
+		 * [0-9]*[:print:](.*)? with the last group's validity dependent
+		 * on the character in [:print:]
+		 */
+
+		if (ch == KEY_RESIZE) {
+			resize(SIGWINCH);
+			ch = 0;
+		} else if (esc_capture && ch == ERR) {
+			buf[pos] = '\0';
+			ch = process_esc_sequence(buf);
+			pos = 0;
+			esc_capture = false;
+			if (ch == ERR) continue;
+		} else if (ch == ERR) {
+			continue;
+		} else if (esc_capture) {
+			buf[pos++] = ch;
+			continue;
+		}
+
+		werase(stdscr);
+
+		if (ch) switch(edit_mode)
 		{
-			case 0:
-				break;
-			case KEY_DOWN: 
-				move_down();
-				break;
-			case KEY_UP: 
-				move_up();
-				break;
-			case KEY_LEFT: 
-				move_left();
-				break;
-			case KEY_RIGHT:
-				move_right();
-				break;
-
-			case KEY_HOME: 
-				curs_x=0; 
-				file_x=0; 
-				break;
-			case KEY_END: 
-				curs_x=cur_line ? cur_line->used : curs_x; 
-				break;
-			case KEY_BACKSPACE:
-				if (edit_mode == EM_EDIT)
-					delete_char(-1);
-				else
-					move_left();
-				break;
-			case KEY_DL:
-				delete_char(0);
-				break;
-			
-			case KEY_ESC:
-				edit_mode = EM_CMD;
-				break;
-
-			case CTRL('C'):
-				exit(EXIT_SUCCESS);
-
-			default:
-				switch(edit_mode)
-				{
-					case EM_EDIT:
-						insert_char(ch);
-						break;
-					case EM_CMD:
-						process_cmd(ch);
-						break;
-					case EM_NULL:
-					case EM_LINE:
-						errx(1, "invalid EM state");
-				}
-				break;
+			case EM_LINE: process_line_ch(ch); break;
+			case EM_EDIT: process_edit_ch(ch); break;
+			case EM_CMD:  process_cmd_ch(ch);  break;
+			case EM_NULL: errx(1, "edit_mode"); 
 		}
 
 		draw();
-		move(25, 0);
-		clrtoeol();
+		wmove(stdscr, max_scr_y+1, 0);
+		wclrtoeol(stdscr);
 
 		switch(edit_mode)
 		{
 			case EM_LINE:
-				{
-					mvprintw(25, 0,
-							":%s",
-							edit_line_buf);
-					move(25, strlen(edit_line_buf)+1);
-				}
+				mvwprintw(stdscr, max_scr_y+1, 0, ":%s", edit_line_buf);
+				wmove(stdscr, max_scr_y+1, strlen(edit_line_buf)+1);
 				break;
 			case EM_CMD:
-				mvprintw(25, 0, "\"%s\"%s %luL",
+				mvwprintw(stdscr, max_scr_y+1, 0, "\"%s\"%s %luL",
 						cur_buffer->name,
 						cur_buffer->modified ? " [+]" : "",
 						cur_buffer->used);
 				print_loc();
 				break;
 			case EM_EDIT:
-				{
-					attron(A_BOLD);
-					mvprintw(25, 0, "-- INSERT --");
-					attroff(A_BOLD);
-					print_loc();
-
-					/*
-					   ssize_t actx = compute_linex();
-					   char at = cur_line->line[actx];
-					   mvprintw(25, 0, 
-					   "-- INSERT -- c={%lu,%lu} f=[%3lu,%3lu/%3lu] "
-					   "l=[%lu/%lu] ax=[%lu] ch@=%c ch=%02x", 
-					   curs_x, curs_y, 
-					   file_x, file_y, cur_buffer->used, 
-					   cur_line ? cur_line->used : 0, 
-					   cur_line ? cur_line->size : 0,
-					   actx,
-					   isprint(at) ? at : '?',
-					   ch);
-					   */
-				}
+				wattron(stdscr, A_BOLD);
+				mvwprintw(stdscr, max_scr_y+1, 0, "-- INSERT --");
+				wattroff(stdscr, A_BOLD);
+				print_loc();
 				break;
-			case EM_NULL:
-				errx(1, "invalid EM state");
+			case EM_NULL: errx(1, "invalid EM state");
 		}
+
 		if (edit_mode != EM_LINE)
-			move(curs_y,curs_x);
-		refresh();
+			wmove(stdscr, curs_y, curs_x);
+
+		wrefresh(stdscr);
+		
+		if (nanosleep(&ts, NULL) == -1 && errno != EINTR)
+			err(1, "nanosleep");
 	}
 }
